8e90a67ba6dd80c9928893b92857345714bd1e28
Sebastian Huber <fhs37229@fh-salzburg.ac.at>
2017-03-05 14:15:09 +0100

    update: data & style cursor on button

--- src/index.js
+++ src/index.js
@@ -8,9 +8,18 @@ class App extends React.Component {
     super();
     this.state = {
       entries: [
-        { id: 0, name: 'Stefanie' },
-        { id: 1, name: 'Sepp' },
-        { id: 2, name: 'Alex' }
+        { id: 0, name: "Michael Andorfer" },
+        { id: 1, name: "Nicola Deufemia" },
+        { id: 2, name: "Alexander Gabriel" },
+        { id: 3, name: "Stefanie Habersatter" },
+        { id: 4, name: "Sebastian Huber" },
+        { id: 5, name: "Vera KARL" },
+        { id: 6, name: "Konrad Kleeberger" },
+        { id: 7, name: "Josef Krabath" },
+        { id: 8, name: "Viktoria Steiner" },
+        { id: 9, name: "Daniel Trojer" },
+        { id: 10, name: "Ryan Wels" },
+        { id: 11, name: "Magdalena Wimmer" }
       ],
       currentName: '',
       activePerson: null
