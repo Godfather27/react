dac686e94dc37c903b2940e16e30699580b8f75b
Sebastian Huber <fhs37229@fh-salzburg.ac.at>
2017-03-05 13:09:33 +0100

    add: filter, update & style

--- src/User.js
+++ src/User.js
@@ -1,20 +1,55 @@
 import React from 'react';
 // const log = console.log
 
-const AddUser = ({newName, onChange, onClick}) => {
-  return <div>
-      <input value={newName} onChange={onChange} type="text" />
-      <button onClick={onClick}>+</button>
+const AddUser = ({currentName, changeCurrentName, addPerson, setActivePerson}) => {
+  return <div className="add-user" onClick={setActivePerson.bind(this, null)}>
+      <input className="pull-left" value={currentName} onChange={changeCurrentName} type="text" />
+      <input type="button" onClick={addPerson} value="+"/>
     </div>
 }
 
-const List = ({entries}) => {
+const List = ({entries, removePerson, updateName, activePerson, setActivePerson}) => {
   return <div>
-      {entries.map((entry, index) => <Person key={index} name={entry}/>)}
+      {
+        entries.map((entry, index) => {
+          return <Person
+              key={index}
+              name={entry}
+              removePerson={removePerson.bind(this, index)}
+              updateName={updateName.bind(this, index)}
+              index={index}
+              activePerson={activePerson}
+              setActivePerson={setActivePerson}
+            />
+        })
+      }
     </div>
 }
 
-const Person = ({name}) => <div className="person">Name: {name}</div>
+const NameDisplay = ({updateName, name, activePerson, index, setActivePerson}) => {
+  if(activePerson === index)
+    return <input type="text" onChange={updateName} value={name}/>
+  else
+    return <p onClick={setActivePerson.bind(this, index)}>{name}</p>
+}
+
+const Person = ({name, removePerson, updateName, activePerson, index, setActivePerson}) => {
+  return <div className="person">
+      <NameDisplay
+        updateName={updateName}
+        name={name}
+        activePerson={activePerson}
+        index={index}
+        setActivePerson={setActivePerson}
+      />
+      <input
+        type="button"
+        className="pull-right"
+        onClick={removePerson}
+        value="x"
+      />
+    </div>
+}
 Person.propTypes = {
   name: React.PropTypes.string
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-import {List, AddUser, Person} from './User.js'
+import {List, AddUser} from './User.js'
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       entries: ['Stefanie', 'Sepp', 'Alex'],
-      currentName: ''
+      currentName: '',
+      activePerson: null
     }
   }
-  addName = () => {
+
+  filtered = () => this.state.entries.filter((e) => e.includes(this.state.currentName))
+  addPerson = () => {
     this.setState({
       entries: [...this.state.entries, this.state.currentName],
       newName: ''
+    })
+  }
+  removePerson = (idx) => {
+    this.setState({
+      entries: this.state.entries.filter((e, i) => i !== idx)
+    })
+  }
+  updateName = (idx, e) => {
+    this.setState({
+      entries: this.state.entries.map((v, i) => {
+        return i === idx ? e.target.value : v
+      })
+    })
+  }
+  setActivePerson = (idx, e) => {
+    this.setState({
+      activePerson: idx
     })
   }
   changeCurrentName = (e)=> this.setState({currentName: e.target.value})
@@ -22,8 +42,19 @@ class App extends React.Component {
   render(){
     return <div>
       <h1>Welcome</h1>
-      <AddUser value={this.state.newName} onChange={this.changeCurrentName} onClick={this.addName}/>
-      <List entries={this.state.entries}/>
+      <AddUser
+        value={this.state.newName}
+        changeCurrentName={this.changeCurrentName}
+        addPerson={this.addPerson}
+        setActivePerson={this.setActivePerson}
+      />
+      <List
+        entries={this.filtered()}
+        removePerson={this.removePerson}
+        updateName={this.updateName}
+        activePerson={this.state.activePerson}
+        setActivePerson={this.setActivePerson}
+      />
     </div>
   }
 }
